64c2cdf73a72a9739745186603184651b9be45c6
Paul Spencer <paul@pagameba.com>
2013-06-07 20:42:57 -0400

    update tests to use mocha, add npm package.json

--- tests/tests.js
+++ tests/tests.js
@@ -26,138 +26,199 @@ var assert = require('assert')
   , map
   , err
   ;
-
-  // test constants
-assert.equal(mapserver.MS_OFF, 0, 'MS_OFF not defined');
-assert.equal(mapserver.MS_ON, 1, 'MS_ON not defined');
-
-// test accessing basic info about the mapserver build
-console.log('mapserver version number: ' + mapserver.getVersionInt());
-console.log('mapserver version information: ' + mapserver.getVersion());
-console.log('Threading support: ' + (parseInt(mapserver.supportsThreads()) == 1 ? 'ON' : 'OFF'));
-
-// quick error test
-err = mapserver.getError();
-assert.equal(err.code, 0, 'should be no errors');
-
-// Test default mapfile pattern (must end in .map)
-assert['throws'](function() { 
-  mapserver.loadMap(path.join(datadir), nomapfile);  
-}, Error, 'attempting to load a non-existent map should throw an error.');
-
-// check error
-err = mapserver.getError();
-assert.equal(err.code, 5, 'Default mapfile pattern test failed')
-
-// test resetErrorList
-mapserver.resetErrorList();
-err = mapserver.getError();
-assert.equal(err.code, 0, 'should be no errors');
-
-// test loadMap with missing file
-assert['throws'](function() { 
-  mapserver.loadMap(path.join(datadir, 'missing.map'), nomapfile);  
-}, Error, 'attempting to load a non-existent map should throw an error.');
-
-err = mapserver.getError();
-assert.equal(err.code, 1, 'Mapserver should complain about a mapfile that does not exist.');
-
-assert.doesNotThrow(function() {
-  map = mapserver.loadMap(path.join(datadir, mapfile),datadir);
-}, Error, 'loading a valid map file should not throw an error.');
-
-// test getting map properties
-assert.equal(map.name, "GMAP_DEMO", 'map name is incorrect');
-assert.equal(map.status, mapserver.MS_ON, 'map status is not ON');
-assert.equal(map.width, 600, 'getting map width failed');
-assert.equal(map.height, 300, 'getting map height failed');
-assert.equal(map.cellsize, 0, 'default map cellsize should be 0, got ' + map.cellsize);
-assert.equal(map.scaledenom, -1, 'default map scaledenom should be -1, got ' + map.scaledenom);
-assert.equal(map.maxsize, 2048, 'getting map maxsize failed');
-assert.equal(map.units, mapserver.MS_DD, 'getting map units failed');
-assert.equal(map.resolution, 72, 'getting map resolution failed');
-assert.equal(map.defresolution, 72, 'getting map defresolution failed');
-assert.equal(map.shapepath, './', 'getting shapepath failed');
-assert.equal(path.normalize(map.mappath), path.normalize(path.join(__dirname, 'data')), 'getting mappath failed');
-assert.equal(map.extent.minx, -180, 'getting map extent minx failed');
-assert.equal(map.extent.miny, -90, 'getting map extent miny failed');
-assert.equal(map.extent.maxx, 180, 'getting map extent maxx failed');
-assert.equal(map.extent.maxy , 90, 'getting map extent maxy failed');
-
-// test effect of recompute on cellsize and scaledenom
-map.recompute();
-assert.equal(map.cellsize, 0.6020066889632107, 'recomputing map cellsize failed, got ' + map.cellsize);
-// assert.equal(map.scaledenom, 34533691.52101404, 'recomputing map scaledenom failed, got '+ map.scaledemon);
-
-// test setting map properties
-map.name = "test_set";
-assert.equal(map.name, "test_set", "map name could not be set");  
-
-map.width = 600;
-assert.equal(map.width, 600, 'setting map width failed');
-
-map.height = 600;
-assert.equal(map.height, 600, 'setting map height failed');
-map.height = 300;
-assert.equal(map.height, 300, 'resetting map height failed');
-
-map.maxsize = 4096;
-assert.equal(map.maxsize, 4096, 'setting map maxsize failed');
-
-map.units = mapserver.MS_FEET;
-assert.equal(map.units, mapserver.MS_FEET, 'setting map units failed');
-
-map.resolution = 96;
-assert.equal(map.resolution, 96, 'setting map defresolution failed');
-
-map.defresolution = 96;
-assert.equal(map.defresolution, 96, 'setting map defresolution failed');
-
-// test map.setExtent()
-map.setExtent(-2500000, -402631, 1872800,3130000);
-assert.equal(map.extent.minx, -2500000, 'setting map extent minx failed');
-assert.equal(map.extent.miny, -402631, 'setting map extent miny failed');
-assert.equal(map.extent.maxx, 1872800, 'setting map extent maxx failed');
-assert.equal(map.extent.maxy, 3130000, 'setting map extent maxy failed');
-map.setExtent(-180, -90, 180, 90);
-
-// test indexed accessor to map.layers
-assert.equal(map.layers.foo, undefined, 'map.layers should not expose arbitrary properties');
-assert.equal(typeof map.layers.length, 'number', 'map.layers.length should be an integer');
-assert.equal(map.layers.length, 3, 'map.layers.length should be 2');
-
-// test accessor for layer name
-assert.notEqual(map.layers[1].name,'test', 'layer name should not be test before we change it');
-map.layers[1].name = 'test';
-assert.equal(map.layers[1].name, 'test', 'layer name should have changed.');
-// layers should be accessible by name too
-assert.equal(map.layers['test'].name, 'test', 'layer should be accessible by name');
-
-console.log('is ' + map.layers['grid'].connectiontype + ' = ' + mapserver.MS_GRATICULE);
-
-console.log('intersection coordinates: ' + util.inspect(map.layers['grid'].getGridIntersectionCoordinates(), false, null, true));
-
-console.log(map.outputformat.name + ':' + map.outputformat.mimetype);
-
-map.drawMap(function(drawError, buffer) {
-  if (drawError) {
-    console.log(drawError);
-    assert.ok(false, 'Error drawing map.');
-  } else {
-    buffer = buffer.slice(0,buffer.length);
-    fs.writeFile(path.join(__dirname, 'test_buffer.png'), buffer, function(err) {
-      if (err) throw err;
-    });
-  }
+  
+beforeEach(function() {
+  mapserver.resetErrorList();
 });
 
-function printError(err) {
-  if (err != undefined) {
-    try {
-      console.log('Error code ' + err.code + ' (' + err.codeStr + ') in ' + err.routine + ': ' + err.message);
-      return true;
-    } catch (e) {
-      return false;
-    }
-  }
-}
+describe('mapserver', function() {
+  it('should exports constants', function() {
+      // test constants
+    assert.equal(mapserver.MS_OFF, 0, 'MS_OFF not defined');
+    assert.equal(mapserver.MS_ON, 1, 'MS_ON not defined');
+  });
+  
+  it('should get some basic info about mapserver', function() {
+    assert.ok(mapserver.getVersionInt());
+    assert.ok(mapserver.getVersion());
+    assert.ok(mapserver.supportsThreads());
+  });
+  
+  it('should have no errors yet', function() {
+    // quick error test
+    var err = mapserver.getError();
+    assert.equal(err.code, 0, 'should be no errors');
+  });
+  
+  it('missing mapfile should throw an error', function() {
+    // Test default mapfile pattern (must end in .map)
+    assert['throws'](function() { 
+      mapserver.loadMap(path.join(datadir), nomapfile);  
+    }, Error, 'attempting to load a non-existent map should throw an error.');
+
+    // check error
+    err = mapserver.getError();
+    assert.equal(err.code, 5, 'Default mapfile pattern test failed');
+
+    // test resetErrorList
+    mapserver.resetErrorList();
+    err = mapserver.getError();
+    assert.equal(err.code, 0, 'should be no errors');
+  });
+  
+  it('should load a valid map file', function() {
+    assert.doesNotThrow(function() {
+      map = mapserver.loadMap(path.join(datadir, mapfile),datadir);
+    }, Error, 'loading a valid map file should not throw an error.');
+  });
+  
+  it('should get basic information from the map object', function() {
+    assert.doesNotThrow(function() {
+      map = mapserver.loadMap(path.join(datadir, mapfile),datadir);
+    }, Error, 'loading a valid map file should not throw an error.');
+
+    assert.equal(map.name, "GMAP_DEMO", 'map name is incorrect');
+    assert.equal(map.status, mapserver.MS_ON, 'map status is not ON');
+    assert.equal(map.width, 600, 'getting map width failed');
+    assert.equal(map.height, 300, 'getting map height failed');
+    assert.equal(map.cellsize, 0, 'default map cellsize should be 0, got ' + map.cellsize);
+    assert.equal(map.scaledenom, -1, 'default map scaledenom should be -1, got ' + map.scaledenom);
+    assert.equal(map.maxsize, 2048, 'getting map maxsize failed');
+    assert.equal(map.units, mapserver.MS_DD, 'getting map units failed');
+    assert.equal(map.resolution, 72, 'getting map resolution failed');
+    assert.equal(map.defresolution, 72, 'getting map defresolution failed');
+    assert.equal(map.shapepath, './', 'getting shapepath failed');
+    assert.equal(path.normalize(map.mappath), path.normalize(path.join(__dirname, 'data')), 'getting mappath failed');
+  });
+  
+  it('should set map properties', function() {
+    assert.doesNotThrow(function() {
+      map = mapserver.loadMap(path.join(datadir, mapfile),datadir);
+    }, Error, 'loading a valid map file should not throw an error.');
+
+    // test setting map properties
+    map.name = "test_set";
+    assert.equal(map.name, "test_set", "map name could not be set");  
+
+    map.width = 600;
+    assert.equal(map.width, 600, 'setting map width failed');
+
+    map.height = 600;
+    assert.equal(map.height, 600, 'setting map height failed');
+    map.height = 300;
+    assert.equal(map.height, 300, 'resetting map height failed');
+
+    map.maxsize = 4096;
+    assert.equal(map.maxsize, 4096, 'setting map maxsize failed');
+
+    map.units = mapserver.MS_FEET;
+    assert.equal(map.units, mapserver.MS_FEET, 'setting map units failed');
+
+    map.resolution = 96;
+    assert.equal(map.resolution, 96, 'setting map defresolution failed');
+
+    map.defresolution = 96;
+    assert.equal(map.defresolution, 96, 'setting map defresolution failed');
+  });
+
+  it('should get the map extent', function() {
+    assert.doesNotThrow(function() {
+      map = mapserver.loadMap(path.join(datadir, mapfile),datadir);
+    }, Error, 'loading a valid map file should not throw an error.');
+    assert.equal(map.extent.minx, -180, 'getting map extent minx failed');
+    assert.equal(map.extent.miny, -90, 'getting map extent miny failed');
+    assert.equal(map.extent.maxx, 180, 'getting map extent maxx failed');
+    assert.equal(map.extent.maxy , 90, 'getting map extent maxy failed');
+  });
+
+  it('should set the map extent', function() {
+    assert.doesNotThrow(function() {
+      map = mapserver.loadMap(path.join(datadir, mapfile),datadir);
+    }, Error, 'loading a valid map file should not throw an error.');
+    map.setExtent(-90, -45, 90, 45);
+    assert.equal(map.extent.minx, -90, 'setting map extent minx failed');
+    assert.equal(map.extent.miny, -45, 'setting map extent miny failed');
+    assert.equal(map.extent.maxx, 90, 'setting map extent maxx failed');
+    assert.equal(map.extent.maxy, 45, 'setting map extent maxy failed');
+  });
+
+  it('should compute cell size and scaledenom', function() {
+    assert.doesNotThrow(function() {
+      map = mapserver.loadMap(path.join(datadir, mapfile),datadir);
+    }, Error, 'loading a valid map file should not throw an error.');
+    // test effect of recompute on cellsize and scaledenom
+    map.recompute();
+    assert.equal(map.cellsize, 0.6020066889632107, 'recomputing map cellsize failed, got ' + map.cellsize);
+    assert.equal(map.scaledenom, 189621444.28093645, 'recomputing map scaledenom failed, got '+ map.scaledemon);
+  });
+
+  it('should have access to map layers', function() {
+    assert.doesNotThrow(function() {
+      map = mapserver.loadMap(path.join(datadir, mapfile),datadir);
+    }, Error, 'loading a valid map file should not throw an error.');
+
+    // test indexed accessor to map.layers
+    assert.equal(map.layers.foo, undefined, 'map.layers should not expose arbitrary properties');
+    assert.equal(typeof map.layers.length, 'number', 'map.layers.length should be an integer');
+    assert.equal(map.layers.length, 3, 'map.layers.length should be 3');
+
+    // test accessor for layer name
+    assert.notEqual(map.layers[1].name,'test', 'layer name should not be test before we change it');
+    map.layers[1].name = 'test';
+    assert.equal(map.layers[1].name, 'test', 'layer name should have changed.');
+    // layers should be accessible by name too
+    assert.equal(map.layers['test'].name, 'test', 'layer should be accessible by name');
+
+    // console.log('is ' + map.layers['grid'].connectiontype + ' = ' + mapserver.MS_GRATICULE);
+  });
+
+  it('should get grid intersection coordinates', function() {
+    assert.doesNotThrow(function() {
+      map = mapserver.loadMap(path.join(datadir, mapfile),datadir);
+    }, Error, 'loading a valid map file should not throw an error.');
+
+    var values = map.layers['grid'].getGridIntersectionCoordinates();
+    assert.ok(values, 'grid intersection result is not an object');
+    
+    assert.ok(Array.isArray(values.left), 'left is not an array');
+    assert.ok(Array.isArray(values.top), 'top is not an array');
+    assert.ok(Array.isArray(values.right), 'right is not an array');
+    assert.ok(Array.isArray(values.bottom), 'bottom is not an array');
+    
+    assert.equal(values.left.length, 15, 'does not have 15 values for the left array');
+    assert.equal(values.top.length, 15, 'does not have 15 values for the top array');
+    assert.equal(values.right.length, 15, 'does not have 15 values for the right array');
+    assert.equal(values.bottom.length, 15, 'does not have 15 values for the bottom array');
+    
+    assert.equal(values.left[0].x, 0, 'first left value is not 0');
+  });
+  
+  it('should have an output format', function() {
+    assert.doesNotThrow(function() {
+      map = mapserver.loadMap(path.join(datadir, mapfile),datadir);
+    }, Error, 'loading a valid map file should not throw an error.');
+    
+    assert.equal(map.outputformat.name, 'png', 'output format name is incorrect');
+    assert.equal(map.outputformat.mimetype, 'image/png', 'output format mimetype is incorrect');
+  });
+  
+  it('should draw a map', function() {
+    assert.doesNotThrow(function() {
+      map = mapserver.loadMap(path.join(datadir, mapfile),datadir);
+    }, Error, 'loading a valid map file should not throw an error.');
+    fs.readFile(path.join(__dirname, 'data', 'test_buffer.png'), function(err, data) {
+      if (err) {
+        throw new Error('failed to load test image');
+      }
+      map.drawMap(function(drawError, buffer) {
+        if (drawError) {
+          util.inspect(drawError, false, 0, true);
+          assert.ok(false, 'Error drawing map.');
+        } else {
+          
+          assert.equal(data.toString('hex'), buffer.toString('hex'), 'map draw differed from sample image');
+        }
+      });
+    });
+  });
+});
