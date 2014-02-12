524d004fdbbb71c59f49e0d407c2e6dc8d088512
Paul Spencer <paul@pagameba.com>
2014-02-11 19:41:03 -0500

    whitespace fixes and correct error message in proint projection

--- src/ms_point.cpp
+++ src/ms_point.cpp
@@ -5,17 +5,17 @@ Persistent<FunctionTemplate> MSPoint::constructor;
 
 void MSPoint::Initialize(Handle<Object> target) {
   HandleScope scope;
-  
+
   constructor = Persistent<FunctionTemplate>::New(FunctionTemplate::New(MSPoint::New));
   constructor->InstanceTemplate()->SetInternalFieldCount(1);
   constructor->SetClassName(String::NewSymbol("Point"));
-  
+
   RW_PROPERTY(constructor, "x", PropertyGetter, PropertySetter);
   RW_PROPERTY(constructor, "y", PropertyGetter, PropertySetter);
-  
+
   NODE_SET_PROTOTYPE_METHOD(constructor, "project", Project);
   NODE_SET_PROTOTYPE_METHOD(constructor, "distanceToPoint", DistanceToPoint);
-  
+
   target->Set(String::NewSymbol("Point"), constructor->GetFunction());
 }
 
@@ -28,7 +28,7 @@ MSPoint::~MSPoint() { }
 Handle<Value> MSPoint::New(const Arguments &args) {
   HandleScope scope;
   MSPoint *obj;
-  
+
   if (!args.IsConstructCall()) {
     return ThrowException(String::New("Cannot call constructor as function, you need to use 'new' keyword"));
   }
@@ -40,16 +40,16 @@ Handle<Value> MSPoint::New(const Arguments &args) {
     obj->Wrap(args.This());
     return args.This();
   }
-  
+
   pointObj *point = (pointObj *)calloc(1, sizeof(pointObj));
   if(!point) {
     return args.This();
   }
-  
+
   if (args.Length() == 1) {
     THROW_ERROR(Error, "Points take 2 arguments, x and y.");
   }
-  
+
   if (args.Length() == 2) {
     REQ_DOUBLE_ARG(0, x);
     REQ_DOUBLE_ARG(1, y);
@@ -59,7 +59,7 @@ Handle<Value> MSPoint::New(const Arguments &args) {
     point->x = -1;
     point->y = -1;
   }
-  
+
   obj = new MSPoint(point);
   obj->Wrap(args.This());
   return args.This();
@@ -95,11 +95,11 @@ Handle<Value> MSPoint::Project(const Arguments &args) {
   Local<Object> obj;
   MSProjection *projIn;
   MSProjection *projOut;
-  
+
   if (args.Length() != 2) {
     THROW_ERROR(Error, "projecting a point requires two projection arguments");
   }
-  
+
   if (!args[0]->IsObject()) {
     THROW_ERROR(TypeError, "first argument to project must be Projection object");
   }
@@ -111,7 +111,7 @@ Handle<Value> MSPoint::Project(const Arguments &args) {
   }
 
   projIn = ObjectWrap::Unwrap<MSProjection>(obj);
-  
+
   if (!args[1]->IsObject()) {
     THROW_ERROR(TypeError, "second argument to project must be Projection object");
   }
@@ -119,13 +119,13 @@ Handle<Value> MSPoint::Project(const Arguments &args) {
   obj = args[1]->ToObject();
 
   if (obj->IsNull() || obj->IsUndefined() || !MSProjection::constructor->HasInstance(obj)) {
-    THROW_ERROR(TypeError, "first argument to project must be Projection object");
+    THROW_ERROR(TypeError, "second argument to project must be Projection object");
   }
 
   projOut = ObjectWrap::Unwrap<MSProjection>(obj);
-  
+
   msProjectPoint(projIn->this_, projOut->this_, point->this_);
-    
+
   return Undefined();
 }
 
@@ -133,11 +133,11 @@ Handle<Value> MSPoint::DistanceToPoint(const Arguments &args) {
   MSPoint *point = ObjectWrap::Unwrap<MSPoint>(args.This());
   Local<Object> obj;
   MSPoint *anotherPoint;
-  
+
   if (args.Length() != 1) {
     THROW_ERROR(Error, "distanceToPoint needs a Point to measure to");
   }
-  
+
   if (!args[0]->IsObject()) {
     THROW_ERROR(TypeError, "distanceToPoint argument must be Point object");
   }
@@ -149,7 +149,7 @@ Handle<Value> MSPoint::DistanceToPoint(const Arguments &args) {
   }
 
   anotherPoint = ObjectWrap::Unwrap<MSPoint>(obj);
-  
+
   int distance = msDistancePointToPoint(point->this_, anotherPoint->this_);
   RETURN_NUMBER(distance);
 }
