66850a792597dd023f3f99a26327760388c246a2
Paul Spencer <paul@pagameba.com>
2014-06-06 14:20:48 -0400

    Add clone method to map object to support simultaneous draw requests.

--- src/ms_map.hpp
+++ src/ms_map.hpp
@@ -25,36 +25,37 @@ public:
   static void Initialize(Handle<Object> target);
   static Handle<Value> New(const Arguments &args);
   static Handle<Value> New(mapObj *map);
-  
+
+  static Handle<Value> Clone(const Arguments& args);
   static Handle<Value> SelectOutputFormat (const Arguments& args);
   static Handle<Value> SetExtent (const Arguments& args);
   static Handle<Value> DrawMap (const Arguments& args);
   static Handle<Value> Save (const Arguments& args);
   static void DrawMapAfter(uv_work_t *req);
   static void DrawMapWork(uv_work_t *req);
-  
+
   static Handle<Value> Recompute (const Arguments& args);
   static Handle<Value> InsertLayer (const Arguments& args);
   static Handle<Value> SetSymbolSet(const Arguments &args);
   static Handle<Value> GetLabelCache(const Arguments &args);
-  
+
   // static Handle<Value> Copy (const Arguments& args);
   static Handle<Value> PropertyGetter (Local<String> property, const AccessorInfo& info);
   static void PropertySetter (Local<String> property, Local<Value> value, const AccessorInfo& info);
-  
+
   MSMap();
   MSMap(mapObj *map);
   inline mapObj *get() { return this_; }
   mapObj *this_;
-  
+
   MSLayers *layers_;
   MSRect *extent;
   MSOutputFormat *outputformat;
   // labelCacheObj labelcache;
-  
+
 private:
   ~MSMap();
-  
+
 };
 
 #endif
